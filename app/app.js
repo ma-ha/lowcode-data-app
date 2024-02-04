4f6ac271813d466e4d13de869215f3d3ba322ff7
MH <m.e.harms@t-online.de>
2024-02-04 07:37:12 +0100

    clean up

--- app/app.js
+++ app/app.js
@@ -4,21 +4,15 @@ const log      = require( './helper/log' ).logger
 const pjson    = require( './package.json' )
 log.info( `Starting ${pjson.name} v${pjson.version} NODE_ENV=${process.env.NODE_ENV}` )
 
-// const db       = require( './db' )
 const appGUI    = require( './gui/gui' )
 const appSec    = require( './gui/app-sec' )
 const appData   = require( './app-dta' )
 const appAPI    = require( './api/api-entity' )
 const userAPI   = require( './api/api-user' )
 const appMgrAPI = require( './api/api-appmgr' )
-
 const appMgrGUI = require( './gui/gui-appmgr' )
-
-const oidc      =  require( './oidc.js' )
-
-// const job      = require( './job' )
-
-const eh = require( './even-hub.js' )
+const oidc      = require( './oidc.js' )
+const eh        = require( './even-hub.js' )
 
 async function start() {
   // await db.init( )
